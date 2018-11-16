d6bd5f2af753f9bf7ec53e30bf0a5fdc4534240b
Shemar Dacosta <dshemar6@gmail.com>
2018-11-16 20:06:20 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function add(a,b){
+  return a + b
+}
+console.log(add(2,2))
+
+function subtract(a,b){
+  return a - b
+}
+console.log(subtract(2,1))
+
+function multiply(a,b){
+  return a * b
+}
+console.log(multply(2,1))
